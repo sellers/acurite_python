353b11724b61ff16dcd7516cdf099e997c041420
sellers <chris.sellers@ithaka.org>
2016-03-06 18:40:03 -0500

    math

--- src/weatherstation.c
+++ src/weatherstation.c
@@ -144,7 +144,7 @@ int getRainCount(char *data){
     return(count);
 }
 float getBaro(char *data){
-    float baro = (data[24]);
+    float baro = (data[24])/2.7;
     //float baro = ((data[23] << 8) + data[24]);
     //float baro = 6.23*(data[23] << 8 | data[24]) - 20402;
     return(baro);
